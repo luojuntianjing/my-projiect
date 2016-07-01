4ff7385a164301c2d49748017ebd0ef463098663
luojuntianjing <2425424990@qq.com>
2016-07-01 21:41:10 +0800

    Unusual

---
+++ Unusual/src/one/Addexception.java
@@ -0,0 +1,8 @@
+package one;
+
+public class Addexception extends Exception{
+	 public Addexception(String message) {        //用来创建指定参数对象
+	        super(message);                             //调用超类构造器
+	    }
+
+}
---
+++ Unusual/src/one/Test.java
@@ -0,0 +1,16 @@
+package one;
+
+public class Test {
+	
+	public static void main(String[] args) {
+    int n=20;
+    try{
+	       if(n>10){
+	       throw new Addexception("Êý×Ö´óÒì³£");
+	       }       
+       }catch(Exception e){
+	    	 e.printStackTrace();  
+       }
+	
+	}
+}
